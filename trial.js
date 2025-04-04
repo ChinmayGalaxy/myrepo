f57ba0fd5bd104a588105d022a2d991ce71c46e7
Chinmay <cmunagekar@galaxe.com>
2025-04-04 20:38:41 +0530

    Fisrt Version

---
+++ trial.js
@@ -0,0 +1,12 @@
+console.log("Hi welcome");
+
+sum = 23 + 34
+
+console.log(sum + " Total");
+
+let list = [1,2,3,4,5]
+
+for(let i=0; i<=list.length-1; i++)
+{
+    console.log("Numbers " + list[i]);
+}
